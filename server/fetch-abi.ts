c917a4dfe5cac393e48dfa99d8b8c7ce8fdee242
João Alexandre Vaz Ferreira <30525975+joaoavf@users.noreply.github.com>
2023-09-16 18:18:29 +0000

    Incredibly basic UI

--- server/fetch-abi.ts
+++ server/fetch-abi.ts
@@ -1,19 +1,19 @@
-export const networks = [{
-    name: 'ethereum',
-    chainId: 1,
-    blockExplorerUrl: 'https://api.etherscan.io/api',
-    apiKey: process.env.ETHERSCAN_API_KEY || '',
-}]
-
+export const networks = {ethereum: 
+    {
+        chainId: 1,
+        blockExplorerUrl: 'https://api.etherscan.io/api',
+        apiKey: process.env.ETHERSCAN_API_KEY || '',
+    }
+}
 
 export type Network = {
-    name: string;
     chainId: number;
     blockExplorerUrl: string;
     apiKey: string;
 }
 
-export async function fetchABI(network: Network, address: string): Promise<any> {
+export async function fetchABI(networkName: string, address: string): Promise<any> {
+    const network = networks[networkName];
     const URL = `${network.blockExplorerUrl}?module=contract&action=getabi&address=${address}&apikey=${network.apiKey}`;
     const response = await fetch(URL);
     const ABI = await response.json();
--- tests/fetch-abi.spec.ts
+++ tests/fetch-abi.spec.ts
@@ -1,8 +1,8 @@
-import { fetchABI, networks } from '../server/fetch-abi';
+import { fetchABI } from '../server/fetch-abi';
 
 describe('fetch-abi', () => {
     it('fetches ABI', async () => {
-        const ABI = await fetchABI(networks[0], '0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413')
+        const ABI = await fetchABI('ethereum', '0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413')
         expect(ABI.length).toBeGreaterThan(0);
     });
 });
