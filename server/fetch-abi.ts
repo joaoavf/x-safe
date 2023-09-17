faa6217aa971d7c53b62a2bc1fbf5784474b5c04
João Alexandre Vaz Ferreira <30525975+joaoavf@users.noreply.github.com>
2023-09-17 15:41:23 +0000

    Supporting contracts w payable methods

--- server/fetch-abi.ts
+++ server/fetch-abi.ts
@@ -24,11 +24,12 @@ export async function fetchABI(networkName: string, address: string): Promise<an
     const URL = `${network.blockExplorerUrl}?module=contract&action=getabi&address=${address}&apikey=${network.apiKey}`;
     const response = await fetch(URL);
     const ABI = await response.json();
+    console.log(JSON.parse(ABI.result))
 
     if (ABI.status === '0') {
         throw new Error(ABI.result);
     } else {
-        const treatedABI = JSON.parse(ABI.result).filter((item: any) => item.type === 'function' && item.constant == false).sort((a, b) => {
+        const treatedABI = JSON.parse(ABI.result).filter((item: any) => item.type === 'function' && (item.constant == false || item.stateMutability === 'nonpayable' || item.stateMutability === 'payable')).sort((a, b) => {
             if (a.name < b.name) {
               return -1;
             }
--- tests/fetch-abi.spec.ts
+++ tests/fetch-abi.spec.ts
@@ -2,7 +2,8 @@ import { fetchABI } from '../server/fetch-abi';
 
 describe('fetch-abi', () => {
     it('fetches ABI', async () => {
-        const ABI = await fetchABI('ethereum', '0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413')
+        const ABI = await fetchABI('ethereum', '0x7a250d5630b4cf539739df2c5dacb4c659f2488d')
+        console.log(ABI)
         expect(ABI.length).toBeGreaterThan(0);
     });
 });
