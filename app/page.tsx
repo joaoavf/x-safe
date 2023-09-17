6399d425473686bc750660cc450f9aff101b59a5
João Alexandre Vaz Ferreira <30525975+joaoavf@users.noreply.github.com>
2023-09-17 05:38:42 +0000

    Making it prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,38 @@
 "use client";
-import { fetchABI } from '@/server/fetch-abi';
-import React, { useState } from 'react';
-import { Web3Button } from '@web3modal/react'
-import { useWalletClient } from 'wagmi'
-import { getSafe, getSafeTx } from '@/server/safe';
+import { fetchABI } from "@/server/fetch-abi";
+import React, { useState } from "react";
+import { Web3Button } from "@web3modal/react";
+import { useWalletClient } from "wagmi";
+import { getSafe, getSafeTx } from "@/server/safe";
 
 const App = () => {
   const [showSafeModal, setShowSafeModal] = useState(false);
   const [safes, setSafes] = useState([]);
-  const [newSafe, setNewSafe] = useState({ blockchain: 'ethereum', address: ''});
+  const [newSafe, setNewSafe] = useState({
+    blockchain: "ethereum",
+    address: "",
+  });
 
   const [showModal, setShowModal] = useState(false);
   const [contracts, setContracts] = useState([]);
-  const [newContract, setNewContract] = useState({ blockchain: 'ethereum', address: '', ABI: [], selectedFunction: ''});
-  
-  const {data: signer} = useWalletClient();
-  const safeAddress = '0xEeac56EFC9ff9806214ba1d0bdB9321953ae3e83';
+  const [newContract, setNewContract] = useState({
+    blockchain: "ethereum",
+    address: "",
+    ABI: [],
+    selectedFunction: "",
+  });
+
+  const { data: signer } = useWalletClient();
+  const safeAddress = "0xEeac56EFC9ff9806214ba1d0bdB9321953ae3e83";
   const safe = getSafe(safeAddress, signer);
 
-  const to = '0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F';
-  const data = '0x';
-  const value = '0';
-  
-  const safeTx =  getSafeTx(safe, to, data, value);
+  const to = "0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F";
+  const data = "0x";
+  const value = "0";
 
-  console.log('safeTx', safeTx);
+  const safeTx = getSafeTx(safe, to, data, value);
+
+  console.log("safeTx", safeTx);
 
   const addContract = async () => {
     const ABI = await fetchABI(newContract.blockchain, newContract.address);
@@ -48,111 +56,167 @@ const App = () => {
   }
 
   return (
-    <div>
-      <Web3Button />
-      <h1>Step 1 - Add Safes</h1>
-      <button onClick={() => setShowSafeModal(true)}>Add New Safe</button>
+    <div className="container mx-auto p-4">
+      <Web3Button className="mb-4" />
+
+      <h1 className="text-2xl font-bold mb-4">Step 1 - Add Safes</h1>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={() => setShowSafeModal(true)}
+      >
+        Add New Safe
+      </button>
       {showSafeModal && (
-        <div>
-          <div className="z-10 w-full items-left justify-between font-mono text-sm lg:flex">
-          </div>
-          <select onChange={(e) => setNewSafe({ ...newContract, blockchain: e.target.value })}>
+        <div className="mt-4">
+          <select
+            className="border p-2 rounded mb-2"
+            onChange={(e) =>
+              setNewSafe({ ...newContract, blockchain: e.target.value })
+            }
+          >
             <option>ethereum</option>
             <option>arbitrum</option>
             <option>polygon</option>
           </select>
           <input
+            className="border p-2 rounded mb-2"
             type="text"
             placeholder="Address"
-            onChange={(e) => setNewSafe({ ...newContract, address: e.target.value })}
+            onChange={(e) =>
+              setNewSafe({ ...newContract, address: e.target.value })
+            }
           />
-          <button onClick={addSafe}>Add Safe</button>
-          <button onClick={() => setShowModal(false)}>Cancel</button>
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded"
+            onClick={addSafe}
+          >
+            Add Safe
+          </button>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded ml-2"
+            onClick={() => setShowModal(false)}
+          >
+            Cancel
+          </button>
         </div>
       )}
 
-    <table>
+      <table className="min-w-full mt-4 border">
         <thead>
-          <tr>
-            <th>Blockchain</th>
-            <th>Safe Address</th>
+          <tr className="bg-gray-200">
+            <th className="py-2 px-4 border">Blockchain</th>
+            <th className="py-2 px-4 border">Safe Address</th>
           </tr>
         </thead>
         <tbody>
           {safes.map((safe, index) => (
             <tr key={index}>
-              <td>{safe.blockchain}</td>
-              <td>{safe.address}</td>
+              <td className="py-2 px-4 border">{safe.blockchain}</td>
+              <td className="py-2 px-4 border">{safe.address}</td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      <h1>Step 2 - Add Contract Calls</h1>
-
-      <button onClick={() => setShowModal(true)}>Add</button>
+      <h1 className="text-2xl font-bold mt-8 mb-4">
+        Step 2 - Add Contract Calls
+      </h1>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+        onClick={() => setShowModal(true)}
+      >
+        Add
+      </button>
       {showModal && (
-        <div>
-          <div className="z-10 w-full items-left justify-between font-mono text-sm lg:flex">
-          </div>
-
-          <select onChange={(e) => setNewContract({ ...newContract, blockchain: e.target.value })}>
+        <div className="mt-4">
+          <div className="z-10 w-full items-left justify-between font-mono text-sm lg:flex"></div>
+          <select
+            className="border p-2 rounded mb-2"
+            onChange={(e) =>
+              setNewContract({ ...newContract, blockchain: e.target.value })
+            }
+          >
             <option>ethereum</option>
             <option>arbitrum</option>
             <option>polygon</option>
           </select>
           <input
+            className="border p-2 rounded mb-2"
             type="text"
             placeholder="Address"
-            onChange={(e) => setNewContract({ ...newContract, address: e.target.value })}
+            onChange={(e) =>
+              setNewContract({ ...newContract, address: e.target.value })
+            }
           />
-          <button onClick={addContract}>Add</button>
-          <button onClick={() => setShowModal(false)}>Cancel</button>
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded"
+            onClick={addContract}
+          >
+            Add
+          </button>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded ml-2"
+            onClick={() => setShowModal(false)}
+          >
+            Cancel
+          </button>
         </div>
       )}
 
-      <table>
+      <table className="min-w-full mt-4 border">
         <thead>
-          <tr>
-            <th>Blockchain</th>
-            <th>Address</th>
-            <th>Functions</th>
-            <th>Inputs</th>
-            <th>Status</th>
+          <tr className="bg-gray-200">
+            <th className="py-2 px-4 border">Blockchain</th>
+            <th className="py-2 px-4 border">Address</th>
+            <th className="py-2 px-4 border">Functions</th>
+            <th className="py-2 px-4 border">Inputs</th>
+            <th className="py-2 px-4 border">Status</th>
           </tr>
         </thead>
         <tbody>
           {contracts.map((contract, index) => (
             <tr key={index}>
-              <td>{contract.blockchain}</td>
-              <td>{contract.address}</td>
-              <td>
-              <select onChange={(e) => updateContract(contract, e.target.value )}>
-                {contract?.ABI?.length > 0 ? contract.ABI.map((contract, index) => (
-                  <option key={index}>{contract.name}</option>
-                )) : <></>}
-              </select>
+              <td className="py-2 px-4 border">{contract.blockchain}</td>
+              <td className="py-2 px-4 border">{contract.address}</td>
+              <td className="py-2 px-4 border">
+                <select
+                  className="border p-1 rounded"
+                  onChange={(e) => updateContract(contract, e.target.value)}
+                >
+                  {contract?.ABI?.length > 0 ? (
+                    contract.ABI.map((contract, index) => (
+                      <option key={index}>{contract.name}</option>
+                    ))
+                  ) : (
+                    <></>
+                  )}
+                </select>
               </td>
-              <td>
-              {contract.selectedFunction !== '' && contract.ABI.map((abiFunction, index) => {
-                if (abiFunction.name === contract.selectedFunction) {
-                  return (
-                    <div key={index}>
-                      {/* Generate input fields for each input in abiFunction.inputs */}
-                      {abiFunction.inputs.map((input, inputIndex) => (
-                        <div key={inputIndex}>
-                          <label>{`${input.name} (${input.type})`}</label>
-                          <input type="text" />
+              <td className="py-2 px-4 border">
+                {contract.selectedFunction !== "" &&
+                  contract.ABI.map((abiFunction, index) => {
+                    if (abiFunction.name === contract.selectedFunction) {
+                      return (
+                        <div key={index}>
+                          {abiFunction.inputs.map((input, inputIndex) => (
+                            <div key={inputIndex} className="mb-2">
+                              <label className="mr-2">{`${input.name} (${input.type})`}</label>
+                              <input
+                                className="border p-1 rounded"
+                                type="text"
+                              />
+                            </div>
+                          ))}
                         </div>
-                      ))}
-                    </div>
-                  );
-                }})
-              }
-            </td>
-            <td>
-              <button>Call</button>
-            </td>
+                      );
+                    }
+                  })}
+              </td>
+              <td className="py-2 px-4 border">
+                <button className="bg-blue-500 text-white px-4 py-2 rounded">
+                  Call
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
